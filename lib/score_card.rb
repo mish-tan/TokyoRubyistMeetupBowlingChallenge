0460b6627daf9d2cd1d1c4f9712e4b5c37f2378f
Michelle <mish.mash.tan@gmail.com>
2026-01-18 14:25:45 +0900

    update score_card so it's ready for calculating full_score

--- lib/score_card.rb
+++ lib/score_card.rb
@@ -1,30 +1,33 @@
 class ScoreCard
-  # Counts the number of pins knocked over from a bowling score-card.
+  # Calculate the full score from a bowling score-card.
   #
   # Args:
   #   score_card: String
   #     A space-separated string of 10 frames.
   #
   # Returns: Integer
-  #   The number of pins knocked over during the game.
+  #   Representing the score of the game.
   #
   # Possible frame scores:
-  #   "X": strike
-  #   "i/": spare
-  #   "ij": open frame
-  #   "-": gutter ball(s)
+  #   "X": strike (10 + following 2 rolls, unless 10th frame then 10)
+  #   "i/": spare (10 + following roll, unless 10th frame then 10)
+  #   "ij": open frame (i+j)
+  #   "-": gutter ball(s) (0)
+  #
+  #   * where i,j are between 1 and 9 and i+j < 10
   #
   # Example input:
-  #   "X 7/ 9- X -8 8/ - 72 X X81"
+  #   "X X X X X X X X X XXX" - the perfect game, scores 300.
+  #   "X 7/ 9- X -8 8/ - 72 X X81" has a score of 140.
   #
   # Note:
   #   You may assume all score-cards are valid.
   #   A single frame scoring "-" means two gutter balls were rolled.
-  #   A strike or spare in the 10th frame unlocks a bonus roll.
+  #   A strike or spare in the 10th frame unlocks a bonus roll, but scores 10 only.
 
-  def self.pins_knocked_over(score_card)
-    total_knocked_over = -1 # TODO: implement logic
+  def self.full_score(score_card)
+    total_score = -1 # TODO: implement logic
 
-    total_knocked_over
+    return total_score
   end
 end
--- spec/score_card_spec.rb
+++ spec/score_card_spec.rb
@@ -1,50 +1,60 @@
 require_relative "../lib/score_card"
 
 RSpec.describe ScoreCard do
-  describe "pins_knocked_over" do
-    it "returns 120 when a perfect game" do
-      expect(described_class.pins_knocked_over("X X X X X X X X X XXX")).to eq(120)
+  describe "full_score" do
+
+    it "returns 300 when a perfect game" do
+      expect(described_class.full_score("X X X X X X X X X XXX")).to eq(300)
     end
 
-    it "returns 105 when all spares and bonus roll is 5" do
-      expect(described_class.pins_knocked_over("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5")).to eq(105)
+
+    it "returns 150 when all spares and bonus roll is 5" do
+      expect(described_class.full_score("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5")).to eq(150)
     end
 
-    it "returns 110 when all spares and bonus roll is a strike" do
+
+    it "returns 155 when all spares and bonus roll is a strike" do
       card = "-/ 1/ 2/ 3/ 4/ 5/ 6/ 7/ 8/ 9/ X"
-      expect(described_class.pins_knocked_over(card)).to eq(110)
+      expect(described_class.full_score(card)).to eq(155)
     end
+
 
     it "returns 0 when all gutters indicated by single dash" do
-      expect(described_class.pins_knocked_over("- - - - - - - - - -")).to eq(0)
+      expect(described_class.full_score("- - - - - - - - - -")).to eq(0)
     end
+
 
     it "returns 0 when all gutters indicated by double dash" do
-      expect(described_class.pins_knocked_over("-- -- -- -- -- -- -- -- -- --")).to eq(0)
+      expect(described_class.full_score("-- -- -- -- -- -- -- -- -- --")).to eq(0)
     end
+
 
     it "returns 0 when all gutters indicated by mix of single and double dash" do
-      expect(described_class.pins_knocked_over("- -- - -- - -- - -- - --")).to eq(0)
+      expect(described_class.full_score("- -- - -- - -- - -- - --")).to eq(0)
     end
+
 
     it "returns 90 when all frames open" do
-      expect(described_class.pins_knocked_over("9- 81 72 63 54 45 36 27 18 -9")).to eq(90)
+      expect(described_class.full_score("9- 81 72 63 54 45 36 27 18 -9")).to eq(90)
     end
+
 
     it "returns 65 when open frames and bonus roll" do
-      expect(described_class.pins_knocked_over("11 21 31 41 51 61 71 81 91 9/X")).to eq(65)
+      expect(described_class.full_score("11 21 31 41 51 61 71 81 91 9/X")).to eq(65)
     end
 
-    it "returns 95 when the example game" do
-      expect(described_class.pins_knocked_over("X 7/ 9- X -8 8/ - 72 X X81")).to eq(95)
+
+    it "returns 140 when the example game" do
+      expect(described_class.full_score("X 7/ 9- X -8 8/ - 72 X X81")).to eq(140)
     end
 
-    it "returns 96 when the last roll is a spare" do
-      expect(described_class.pins_knocked_over("X 7/ 9- X -8 8/ - 72 X X8/")).to eq(96)
+
+    it "returns 141 when the last roll is a spare" do
+      expect(described_class.full_score("X 7/ 9- X -8 8/ - 72 X X8/")).to eq(141)
     end
 
-    it "returns 97 when the last frame contains a spare" do
-      expect(described_class.pins_knocked_over("X 7/ 9- X -8 8/ - 72 X 8/1")).to eq(97)
+    it "returns 124 when the last frame contains a spare" do
+      expect(described_class.full_score("X 7/ 9- X -8 8/ - 72 X 8/1")).to eq(124)
     end
   end
 end
